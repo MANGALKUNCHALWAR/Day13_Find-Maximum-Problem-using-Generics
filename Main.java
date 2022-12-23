7bc59c52baceb7231b3154e4a026a2de421b1ba9
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-23 17:40:20 +0530

    Welcome to the Find Maximum Problem using Generics

---
+++ Main.java
@@ -0,0 +1,7 @@
+package assignment13.FindMaximumProblemusingGenerics;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Find Maximum Problem using Generics ");
+    }
+}
